5c15323fb9f4a52162f13212be73a51e1723ef56
Santiago Macías <santiagomacias8@hotmail.com>
2021-07-21 12:52:02 -0500

    Adding updates

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -78,24 +78,25 @@ keys = [
     Key([mod], "l", lazy.layout.right()),
     Key([mod], "j", lazy.layout.down()),
     Key([mod], "k", lazy.layout.up()),
+    Key([mod], "p", lazy.spawn("rofi -show drun")),
+    Key([mod], "e", lazy.spawn("ibus emoji")),
+    Key([mod], "b", lazy.spawn(browser)),
+    Key([mod], "f", lazy.spawn(file_manager)),
     Key([mod], "space", lazy.layout.next()),
     Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    Key([mod, "shift"], "c", lazy.window.kill()),
+    Key([mod, "shift"], "q", lazy.spawn("rofi -show power-menu -modi power-menu:/home/santiago/.local/bin/rofi-power-menu")),
+    Key(['shift'], 'Print', lazy.function(screenshot(selection=False, full=True))),
     Key([mod, "control"], "l", lazy.spawn("dm-tool switch-to-greeter")),
+    Key([mod, "control"], "r", lazy.restart()),
     Key([mod], "Return", lazy.spawn(terminal)),
     Key([mod], "Tab", lazy.next_layout()),
-    Key([mod, "shift"], "c", lazy.window.kill()),
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod], "p", lazy.spawn("rofi -show drun")),
-    Key([mod, "shift"], "q", lazy.spawn("rofi -show power-menu -modi power-menu:/home/santiago/.local/bin/rofi-power-menu")),
     Key([mod], "Right", lazy.function(brightness("up"))),
     Key([mod], "Left", lazy.function(brightness("down"))),
-    Key([mod], "b", lazy.spawn(browser)),
-    Key(['shift'], 'Print', lazy.function(
-        screenshot(selection=False, full=True))),
-    Key([], 'Print', lazy.function(screenshot(selection=True, full=False))),
+    Key([], 'Print', lazy.function(screenshot(selection=True, full=False)))
 ]
 
 groups = [Group(i) for i in "123456"]
@@ -187,7 +188,8 @@ screens = [
                     font="SF Pro Display",
                     fontsize=18,
                     foreground="#f8f8f2",
-                    format="{MemUsed: } /{MemTotal: } MB"
+                    format="{MemUsed: .0f} MB / {MemTotal: .0f} MB",
+		    max_chars=20
                 ),
                 widget.TextBox(
                     text="  ",
